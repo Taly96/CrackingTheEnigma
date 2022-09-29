57589e034a61bf8723eb6f3b7b6b8c4a54a858e7
Taly96 <talicht96@gmail.com>
2022-09-29 14:55:48 +0300

    first version

---
+++ CrackingTheEnigmaClientServer/UBoat/src/view/header/UBoatHeaderController.java
@@ -0,0 +1,21 @@
+package view.header;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UBoatHeaderController {
+
+    @FXML
+    private Button buttonLoad;
+
+    @FXML
+    private TextField textFieldXMLFile;
+
+    @FXML
+    void onLoadXMLFile(ActionEvent event) {
+
+    }
+
+}
