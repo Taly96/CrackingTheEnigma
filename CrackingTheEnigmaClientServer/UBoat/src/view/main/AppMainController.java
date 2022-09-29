4d4c04aee177470fae96429a539754348645c140
Taly96 <talicht96@gmail.com>
2022-09-29 15:12:15 +0300

    first version

---
+++ CrackingTheEnigmaClientServer/UBoat/src/view/main/AppMainController.java
@@ -0,0 +1,11 @@
+package view.main;
+
+import javafx.stage.Stage;
+
+public class AppMainController {
+
+    private Stage primaryStage = null;
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+}
